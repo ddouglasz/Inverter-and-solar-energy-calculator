29af9bd6a1f839bc488fc0d7f26ada0bef570a32
ddouglasz <douglasegiemeh@gmail.com>
2021-06-02 13:38:42 +0100

    switch to user dropdown for loads options

--- estimate-clientside/src/components/saveItemForm.js
+++ estimate-clientside/src/components/saveItemForm.js
@@ -1,62 +1,62 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 //components
-import Input from './input'
-import Button from './buttons'
+import Input from "./input";
+import Button from "./buttons";
 
+//Constants
+import { LOADS } from "../helpers/loads";
 
-const SaveItemForm = ({saveItem, updateChange}) => {
-	
-	// const [powerEstimate, setPowerEstimate] = useState('');
-	const [power_item, setPowerItem] = useState('');
-	const [power_value, setPowerValue] = useState(0);
-	const [value, setValue] = useState('0');
+const SaveItemForm = ({ saveItem, updateChange }) => {
+  // const [powerEstimate, setPowerEstimate] = useState('');
+  const [power_item, setPowerItem] = useState("");
+  const [power_value, setPowerValue] = useState(0);
+  const [value, setValue] = useState("0");
 
-	
-	function updatePowerItem(event) {
+  function updatePowerItem(event) {
+    var index = event.nativeEvent.target.selectedIndex;
     event.preventDefault();
-	  setPowerItem(event.target.value)
-	}
-	
-	function upDatePowerValue(event) {
-    event.preventDefault()
-	  setPowerValue(event.target.value)
-	}
+    setPowerItem(event.nativeEvent.target[index].text);
+    setPowerValue(event.target.value);
+  }
 
-	return (
-		<div>
-       <form className="input-field"  
+  return (
+    <div>
+      <form
+        className="input-field"
         onSubmit={(event) => {
-        event.preventDefault()
-				// saveItem(value)
+          event.preventDefault();
+          // saveItem(value)
+          setValue("");
+        }}
+      >
+        <br />
+        <label htmlFor="loads">Select an Item:</label>
+        <select
+          name="loads"
+          id="loads"
+          onChange={updatePowerItem}
+          value={value}
+        >
+          {LOADS.map((load) => (
+            <option key={load.item} value={load.rating}>
+              {load.item}
+            </option>
+          ))}
+        </select>
+        <br />
 
-				setValue('')
-       }}>
-				<Input
-					placeholder='name of appliance'
-					type='text'
-					name='input'
-					value={power_item}
- 					onChange={updatePowerItem}
- 				/>
-				<Input
-					placeholder='Add appliance rating'
-					type='number'
-					name='input'
-					value={power_value}
- 					onChange={upDatePowerValue}
-				/>
-				<Button
-					name='Generate estimate'
-					type='submit'
-					onclick={(item) => {
-						item = {power_item, power_value}
-						saveItem(item)
-					}}
-				/>
-			</form>
-		</div>
-	)
- }
+        <Button
+          name="Generate estimate"
+          type="submit"
+          onclick={(item) => {
+            item = { power_item, power_value };
+            saveItem(item);
+          }}
+        />
+      </form>
+    </div>
+  );
+};
 
-export default SaveItemForm
+export default SaveItemForm;
---
+++ estimate-clientside/src/helpers/loads.js
@@ -0,0 +1,7 @@
+export const LOADS = [
+    {item: '5W Bulb', rating: 5},
+    {item: '7W Bulb', rating: 7},
+    {item: '9W Bulb', rating: 9},
+    {item: '12W Bulb', rating: 12},
+    {item: '15W Bulb', rating: 15}
+]
